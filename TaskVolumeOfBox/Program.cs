0cd73a32ab3e901f0960f4258a6cfbb3d67cb222
QemerS <tu201806169@code.edu.az>
2021-11-01 02:25:51 +0400

    Add project files.

---
+++ TaskVolumeOfBox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskVolumeOfBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sizes box = new Sizes();
+           
+            int a = box.CalculateVolume(1,2,3);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ TaskVolumeOfBox/Sizes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskVolumeOfBox
+{
+    class Sizes
+    {
+        public byte width;
+        public byte length;
+        public byte height;
+
+        public int CalculateVolume(byte width,byte length, byte height)
+        {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+            return width * length * height;
+        }
+
+    }
+}
